0b5a8a58a8b9bac75e51fe648d493ad79a81ecdf
CkMadhavan <madhavan.champion@gmail.com>
2020-09-20 23:20:49 +0530

    Full

--- git_push.sh
+++ git_push.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 read name
-cd /Downloads/chatbot-master/
 git add .
-git commit -m name
+git commit -m $name
 git push -f heroku master
 git push -f -u origin master
-zenity --info --text="Done Commiting" --title="Done Commiting" --ok-label="Close"
-
